b52579f118a2245e508866754bae05a1c22afb87
Zeuxis Lo <seekstudio@gmail.com>
2016-03-21 19:10:27 +0800

    add home layout

--- app/pages/home/home.js
+++ app/pages/home/home.js
@@ -1,7 +1,29 @@
-import {Page} from 'ionic-angular';
-
+import { NavController } from 'ionic-angular';
+import { Page } from 'ionic-angular';
 
 @Page({
-  templateUrl: 'build/pages/home/home.html'
+    templateUrl: 'build/pages/home/home.html'
 })
-export class HomePage {}
+export class HomePage {
+
+    static get parameters() {
+        return [
+            [NavController]
+        ];
+    }
+
+    constructor(nav) {
+        this.nav = nav;
+    }
+
+    signIn(event) {
+        console.log('signIn Called');
+        console.log(this.username);
+        console.log(this.password);
+    }
+
+    signUp(event) {
+
+    }
+
+}
